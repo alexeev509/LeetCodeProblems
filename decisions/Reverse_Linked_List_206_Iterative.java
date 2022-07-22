b57e30384efccbd213f144e471d7f3080c7035fd
alexeev509 <32396885+alexeev509@users.noreply.github.com>
2022-07-22 20:24:52 +0300

    Update Reverse_Linked_List_206_Iterative.java

--- decisions/Reverse_Linked_List_206_Iterative.java
+++ decisions/Reverse_Linked_List_206_Iterative.java
@@ -1,1 +1,28 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//https://leetcode.com/problems/reverse-linked-list
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode prev = head;
+        ListNode next = prev.next;
+        prev.next = null;
+        while(next != null){
+          ListNode temp = next.next;
+          next.next = prev;
+          prev = next;
+          next = temp;
+        }
+        return prev;
+    }
+}
